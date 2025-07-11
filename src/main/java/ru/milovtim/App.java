994bce35c1a0806be699cdc9fb4e7356c9564260
Timur Milovanov <milovtim@ya.ru>
2025-07-11 18:52:08 +0300

    add PID listener (application.pid file output)

--- src/main/java/ru/milovtim/App.java
+++ src/main/java/ru/milovtim/App.java
@@ -2,10 +2,13 @@ package ru.milovtim;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(App.class);
+        app.addListeners(new ApplicationPidFileWriter());
         SpringApplication.run(App.class, args);
     }
 }
