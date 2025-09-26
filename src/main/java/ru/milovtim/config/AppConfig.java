55de7af7730f54a3ace7c237d5b0fdab87075f74
Timur Milovanov <milovtim@ya.ru>
2025-09-26 19:10:08 +0300

    * move schedule to config * profile for night work

--- src/main/java/ru/milovtim/config/AppConfig.java
+++ src/main/java/ru/milovtim/config/AppConfig.java
@@ -1,11 +1,23 @@
 package ru.milovtim.config;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
-@ConfigurationProperties(prefix = "app")
+import java.time.LocalTime;
+import java.util.Map;
+
 @Component
+@ConfigurationProperties(prefix = "app")
+@Setter @Getter
 public class AppConfig {
 
+    private Map<String, AsicWorkPeriod> workPeriods;
 
+    @Setter @Getter
+    public static class AsicWorkPeriod {
+        private LocalTime start;
+        private LocalTime stop;
+    }
 }
--- src/main/java/ru/milovtim/config/JobsConfig.java
+++ src/main/java/ru/milovtim/config/JobsConfig.java
@@ -1,9 +1,12 @@
 package ru.milovtim.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.quartz.*;
 import org.quartz.core.jmx.JobDataMapSupport;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.milovtim.config.AppConfig.AsicWorkPeriod;
 import ru.milovtim.domain.MinerModeAction.MinerMode;
 import ru.milovtim.service.job.MinerModeJob;
 import ru.milovtim.service.job.MinerRestartJob;
@@ -16,10 +19,10 @@ import static ru.milovtim.domain.MinerModeAction.MinerMode.SLEEP;
 
 
 @Configuration
+@Slf4j
 public class JobsConfig {
 
     public static final String PERMANENT_GROUP = "PERMANENT";
-
 
     @Bean
     JobDetail asic1RestartJobDetail() {
@@ -65,43 +68,66 @@ public class JobsConfig {
 
     //-------------****************************************************
 
+
+    private AsicWorkPeriod asic1WorkPeriod;
+
+    @Value("#{appConfig.workPeriods['asic1']}")
+    public void setAsic1WorkPeriod(AsicWorkPeriod appConf) {
+        this.asic1WorkPeriod = appConf;
+    }
+
     @Bean
     Trigger asic1NightExec() {
+        log.info("Asic1 mode=NORMAL trigger for {}:{}", asic1WorkPeriod.getStart().getHour(), asic1WorkPeriod.getStart().getMinute());
         return TriggerBuilder.newTrigger()
                 .forJob("asic1_mode_NORMAL", PERMANENT_GROUP)
                 .withIdentity("asic1NightTrigger", PERMANENT_GROUP)
-                .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(22, 50)
+                .withSchedule(CronScheduleBuilder
+                        .dailyAtHourAndMinute(asic1WorkPeriod.getStart().getHour(), asic1WorkPeriod.getStart().getMinute())
                         .inTimeZone(TimeZone.getTimeZone("Europe/Moscow")))
                 .build();
     }
 
     @Bean
     Trigger asic1MorningExec() {
+        log.info("Asic1 mode=SLEEP trigger for {}:{}", asic1WorkPeriod.getStop().getHour(), asic1WorkPeriod.getStop().getMinute());
         return TriggerBuilder.newTrigger()
                 .forJob("asic1_mode_SLEEP", PERMANENT_GROUP)
                 .withIdentity("asic1MorningTrigger", PERMANENT_GROUP)
-                .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(7, 0)
+                .withSchedule(CronScheduleBuilder
+                        .dailyAtHourAndMinute(asic1WorkPeriod.getStop().getHour(), asic1WorkPeriod.getStop().getMinute())
                         .inTimeZone(TimeZone.getTimeZone("Europe/Moscow")))
                 .build();
     }
 
 
+    private AsicWorkPeriod asic2WorkPeriod;
+
+    @Value("#{appConfig.workPeriods['asic2']}")
+    public void setAsic2WorkPeriod(AsicWorkPeriod awp) {
+        this.asic2WorkPeriod = awp;
+    }
+
     @Bean
     Trigger asic2NightExec() {
+        log.info("Asic2 mode=NORMAL trigger for {}:{}", asic2WorkPeriod.getStart().getHour(), asic2WorkPeriod.getStart().getMinute());
         return TriggerBuilder.newTrigger()
                 .forJob("asic2_mode_NORMAL", PERMANENT_GROUP)
                 .withIdentity("asic2NightTrigger", PERMANENT_GROUP)
-                .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(23, 0)
+                .withSchedule(CronScheduleBuilder
+                        .dailyAtHourAndMinute(asic2WorkPeriod.getStart().getHour(), asic2WorkPeriod.getStart().getMinute())
                         .inTimeZone(TimeZone.getTimeZone("Europe/Moscow")))
                 .build();
     }
 
     @Bean
     Trigger asic2MorningExec() {
+        log.info("Asic2 mode=SLEEP trigger for {}:{}", asic2WorkPeriod.getStop().getHour(), asic2WorkPeriod.getStop().getMinute());
         return TriggerBuilder.newTrigger()
                 .forJob("asic2_mode_SLEEP", PERMANENT_GROUP)
                 .withIdentity("asic2MorningTrigger", PERMANENT_GROUP)
-                .withSchedule(CronScheduleBuilder.dailyAtHourAndMinute(7, 1)
+                .withSchedule(CronScheduleBuilder
+                        .dailyAtHourAndMinute(asic2WorkPeriod.getStop().getHour(), asic2WorkPeriod.getStop().getMinute())
                         .inTimeZone(TimeZone.getTimeZone("Europe/Moscow")))
                 .build();
     }
